a41f51801ec6a49f13df4fbb9f72ceced9c0064b
Tom Smith <wisdev53548@hotmail.com>
2018-01-20 11:22:50 -0600

    Clarifai added added and working, but face-detection not yet enabled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 // import Particles from 'react-particles-js'
+import Clarifai from 'clarifai'
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import './App.css'
 
 // const particlesOptions = {
@@ -17,6 +19,10 @@ import './App.css'
 //     }
 //   }
 // }
+
+const app = new Clarifai.App({
+ apiKey: 'aa88288ad84443898d2e06a4709885d6'
+})
 
 class App extends Component {
   constructor() {
@@ -32,6 +38,14 @@ class App extends Component {
 
   onButtonClick = () => {
     console.log('click')
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+      function(response) {
+        console.log(response)
+      },
+      function(err) {
+        // there was an error
+      }
+    ); 
   }
 
   render() {
@@ -44,6 +58,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonClick={this.onButtonClick} />
+        <FaceRecognition />
       </div>
     );
   }
