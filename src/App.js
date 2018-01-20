b023815c24ef11f7f5cbb2c00cdc952440e85446
Tom Smith <wisdev53548@hotmail.com>
2018-01-20 12:47:39 -0600

    now using face-detection and uploading images, not yet outputting borders utilizing face-detection

--- src/App.js
+++ src/App.js
@@ -39,9 +39,9 @@ class App extends Component {
 
   onButtonClick = () => {
     this.setState({imageUrl: this.state.input})
-    app.models.predict(Clarifai.COLOR_MODEL, this.state.input).then(
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
       function(response) {
-        console.log(response)
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
       },
       function(err) {
         // there was an error
