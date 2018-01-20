45772e758e9b286ffda874726b9296b7e2ba921f
Tom Smith <wisdev53548@hotmail.com>
2018-01-20 10:19:53 -0600

    disabled particles because it was causing issues this morning

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
 import React, { Component } from 'react'
-import Particles from 'react-particles-js'
+// import Particles from 'react-particles-js'
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
 import './App.css'
 
-const particlesOptions = {
-  particles: {
-    number: {
-      value: 30,
-      density: {
-        enable: true,
-        value_area: 200
-      }
-    }
-  }
-}
+// const particlesOptions = {
+//   particles: {
+//     number: {
+//       value: 30,
+//       density: {
+//         enable: true,
+//         value_area: 200
+//       }
+//     }
+//   }
+// }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = event => {
+    console.log(event.target.value)
+  }
+
+  onButtonClick = () => {
+    console.log('click')
+  }
+
   render() {
     return (
       <div className="App">
-        <Particles className='particles'
+        {/*<Particles className='particles'
           params={particlesOptions}
-        />
+        />*/}
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonClick={this.onButtonClick} />
       </div>
     );
   }
