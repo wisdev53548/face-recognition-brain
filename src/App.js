2c29b18b11053f61921e6d9aafc0dacc4b6acaae
Tom Smith <wisdev53548@hotmail.com>
2018-01-20 11:49:36 -0600

    color detection working

--- src/App.js
+++ src/App.js
@@ -28,17 +28,18 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: ''
+      input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = event => {
-    console.log(event.target.value)
+    this.setState({input: event.target.value})
   }
 
   onButtonClick = () => {
-    console.log('click')
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(Clarifai.COLOR_MODEL, this.state.input).then(
       function(response) {
         console.log(response)
       },
@@ -58,7 +59,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonClick={this.onButtonClick} />
-        <FaceRecognition />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const FaceRecognition = () => {
+const FaceRecognition = ({ imageUrl }) => {
 	return (
 		<div className='center'>
-			<img alt='sample image' src={'https://samples.clarifai.com/face-det.jpg'} />
+			<img alt='sample' src={imageUrl} />
 		</div>
 	)
 }
