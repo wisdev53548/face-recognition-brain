587fca1aaeb09718968aab0876e99905a40edb67
Tom Smith <wisdev53548@hotmail.com>
2018-01-20 13:18:09 -0600

    started adding function to calculate the location of individual faces

--- src/App.js
+++ src/App.js
@@ -29,8 +29,17 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = data => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width)
+    const height = Number(image.height)
+    console.log(width, height)
   }
 
   onInputChange = event => {
@@ -39,13 +48,9 @@ class App extends Component {
 
   onButtonClick = () => {
     this.setState({imageUrl: this.state.input})
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-      },
-      function(err) {
-        // there was an error
-      }
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => this.calculateFaceLocation(response)
+      .catch(err => console.log(err))
     ); 
   }
 
