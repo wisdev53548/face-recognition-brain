5e1609ddf66192e30e295dcc9415390b6906b25d
Tom Smith <wisdev53548@hotmail.com>
2018-01-21 09:28:16 -0600

    Added registration component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 // import Particles from 'react-particles-js'
 import Clarifai from 'clarifai'
 import Navigation from './components/Navigation/Navigation'
+import Register from './components/Register/Register'
 import Signin from './components/Signin/Signin'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
@@ -77,14 +78,18 @@ class App extends Component {
           params={particlesOptions}
         />*/}
         <Navigation onRouteChange={this.onRouteChange} />
-        { this.state.route === 'signin' 
-          ? <Signin onRouteChange={this.onRouteChange} />
-          : <div> 
+        { this.state.route === 'home' 
+          ? <div> 
               <Logo />
               <Rank />
               <ImageLinkForm onInputChange={this.onInputChange} onButtonClick={this.onButtonClick} />
               <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
             </div>
+          : (
+              this.state.route === 'signin' 
+              ? <Signin onRouteChange={this.onRouteChange} />
+              : <Register onRouteChange={this.onRouteChange} />
+            )
         } 
       </div>
     );
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -8,11 +8,11 @@ const Signin = ({ onRouteChange }) =>  {
 			    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 			      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
 			      <div className="mt3">
-			        <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+			        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
 			        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
 			      </div>
 			      <div className="mv3">
-			        <label className="db fw6 lh-copy f6" for="password">Password</label>
+			        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
 			        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
 			      </div>
 			    </fieldset>
@@ -24,7 +24,7 @@ const Signin = ({ onRouteChange }) =>  {
 			      	value="Sign in" />
 			    </div>
 			    <div className="lh-copy mt3">
-			      <a href="#0" className="f6 link dim black db">Register</a>
+			      <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
 			    </div>
 			  </form>
 			</main>
