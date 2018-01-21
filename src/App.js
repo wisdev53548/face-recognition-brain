13d2fd70373c18adfb74a12d1ac0131c7296504d
Tom Smith <wisdev53548@hotmail.com>
2018-01-21 08:54:20 -0600

    Fixed face detection. It is now highlighting correctly.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 // import Particles from 'react-particles-js'
 import Clarifai from 'clarifai'
 import Navigation from './components/Navigation/Navigation'
+import Signin from './components/Signin/Signin'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
@@ -30,7 +31,8 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box: {}
+      box: {},
+      route: 'signin'
     }
   }
 
@@ -71,10 +73,15 @@ class App extends Component {
           params={particlesOptions}
         />*/}
         <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonClick={this.onButtonClick} />
-        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
+        { this.state.route === 'signin' 
+          ? <Signin />
+          : <div> 
+              <Logo />
+              <Rank />
+              <ImageLinkForm onInputChange={this.onInputChange} onButtonClick={this.onButtonClick} />
+              <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
+            </div>
+        } 
       </div>
     );
   }
